4589080638a7cb78c259b7bbb95f3b85c1a90276
alanbrilliant <abrilliant2017@gmail.com>
2019-02-03 15:12:16 -0800

    Did let for variables

--- lab2/code-interp/interp.ml
+++ lab2/code-interp/interp.ml
@@ -9,11 +9,11 @@ let want_dump = ref false
 
 let interp_memref_var memref = 
 	Hashtbl.find Tables.variable_table memref
-
+ 
 let rec eval_expr (expr : Absyn.expr) : float = match expr with
     | Number number -> number
     | Memref memref -> match memref with 
-		|Absyn.Arrayref (v,e) -> interp_memref_arr memref 
+		|Absyn.Arrayref (v,e) -> unimpl "no arrays yet sorry"(*interp_memref_arr memref  *)
 		|Absyn.Variable varian -> eval_expr (interp_memref_var memref)
     | Unary (oper, expr) -> (Hashtbl.find Tables.unary_fn_table oper) (eval_expr expr) 
     | Binary (oper, expr1, expr2) -> (Hashtbl.find Tables.binary_fn_table oper) (eval_expr expr1) (eval_expr expr2)
@@ -37,15 +37,19 @@ let interp_input (memref_list : Absyn.memref list) =
              (print_string "End_of_file"; print_newline ())
     in List.iter input_number 
 
-let interp_let memref, expr = 
-	Hashtbl.add Tables.variable_table memref expr
+let interp_let (memref : Absyn.memref) = match memref with 
+	|Absyn.Arrayref (var, expr) ->unimpl "no arrays yet"
+	|Absyn.Variable var -> Hashtbl.add Tables.variable_table memref expr
+	
+	
+	
 	
 	
 	
 	
 let interp_stmt (stmt : Absyn.stmt) = match stmt with
     | Dim (ident, expr) -> unimpl "Dim (ident, expr)"
-    | Let (memref, expr) -> interp_let (memref, expr) unimpl "Let (memref, expr)"
+    | Let (memref, expr) -> interp_let memref expr
     | Goto label -> unimpl "Goto label"
     | If (expr, label) -> unimpl "If (expr, label)"
     | Print print_list -> interp_print print_list
