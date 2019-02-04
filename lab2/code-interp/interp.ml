372ac309a82bbd14c1d2f7b16eed8f8f894bc4fc
alanbrilliant <abrilliant2017@gmail.com>
2019-02-03 23:31:12 -0800

    GOTO initial test

--- lab2/code-interp/interp.ml
+++ lab2/code-interp/interp.ml
@@ -33,7 +33,7 @@ let interp_print (print_list : Absyn.printable list) =
            in print_string (Str.replace_first regex "\\1" string)
          | Printexpr expr ->
            print_float (eval_expr expr))
-    in (List.iter print_item print_list; print_newline ())
+    in (List.iter print_item print_list; print_newline ()); ();
 
 let interp_input (memref_list : Absyn.memref list) =
     let input_number memref =
@@ -41,23 +41,24 @@ let interp_input (memref_list : Absyn.memref list) =
              in (print_float number; print_newline ())
         with End_of_file -> 
              (print_string "End_of_file"; print_newline ())
-    in List.iter input_number; () 
+    in List.iter input_number; (); 
 
 let interp_let (memref : Absyn.memref) (expr : Absyn.expr) =( match memref with 
 	|Absyn.Arrayref (v,e) -> Array.set (Hashtbl.find Tables.array_table v)(int_of_float (eval_expr e)) (eval_expr expr)
-	|Absyn.Variable var -> Hashtbl.add Tables.variable_table var (eval_expr expr))
+	|Absyn.Variable var -> Hashtbl.add Tables.variable_table var (eval_expr expr)); ();
 	
 	
 let interp_dim (ident : Absyn.ident) (expr : Absyn.expr) = 
-	Hashtbl.add Tables.array_table ident (Array.make (int_of_float (eval_expr expr)) 0.)
+	Hashtbl.add Tables.array_table ident (Array.make (int_of_float (eval_expr expr)) 0.); ();
 	
-	
+let interp_goto label =
+		Hashtbl.find Tables.label_table label; ();
 	
 	
 let interp_stmt (stmt : Absyn.stmt) = match stmt with
     | Dim (ident, expr) ->  interp_dim ident expr
     | Let (memref, expr) -> interp_let memref expr
-    | Goto label -> unimpl "Goto label"
+    | Goto label -> unimpl interp_goto label
     | If (expr, label) -> unimpl "If (expr, label)"
     | Print print_list -> interp_print print_list
     | Input memref_list -> interp_input memref_list
@@ -66,7 +67,10 @@ let rec interpret (program : Absyn.program) = match program with
     | [] -> ()
     | firstline::otherlines -> match firstline with
       | _, _, None -> interpret otherlines
-      | _, _, Some stmt -> (interp_stmt stmt; interpret otherlines)
+      | _, _, Some stmt -> (let stmt_return = interp_stmt stmt in 
+		if stmt_return = None
+		then interpret otherlines
+		else interpret stmt_return)
 
 let interpret_program program =
     (Tables.init_label_table program; 
